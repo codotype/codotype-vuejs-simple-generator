f420071f14b69dbe4f8ed51eb24b68125fff16eb
Alexander Schwartzberg <aeksco@gmail.com>
2019-07-05 23:14:25 -0400

    Updated settings page

--- generator/base/templates/src/modules/main/pages/settings/index.vue
+++ generator/base/templates/src/modules/main/pages/settings/index.vue
@@ -16,7 +16,10 @@
       <div class="container">
         <div class="content">
 
-          <h1 class="title">Import Data</h1>
+          <h2>Import Data</h2>
+          <p class="is-size-6">
+            Import a <code>.json</code> file containing your data.
+          </p>
           <div class="file">
             <label class="file-label">
               <input
@@ -37,25 +40,31 @@
             </label>
           </div>
 
-          <small class="text-success" v-if="imported && importValid">
+          <small class="text-success" v-if="importSuccess">
             <i class="fa fa-check-circle"></i>
-            Imports the {{importJson.label}} Blueprint with {{importJson.schemas.length}} models
+            Successfully imported data
           </small>
 
-          <small class="text-danger" v-if="imported && !importValid">
-            <i class="fa fa-times-circle"></i>
-            Invalid blueprint format
+          <small class="text-danger" v-if="importError">
+            <i class="fa fa-times-times"></i>
+            Error importing data
           </small>
 
-          <h1 class="title">Export Data</h1>
+          <h2>Export Data</h2>
+          <p class="is-size-6">
+            Export your data as a <code>.json</code> file.
+          </p>
           <button class="button is-success" @click="exportData()">
-            <i class="fa fa-fw fa-plus"></i>
+            <i class="fa fa-fw fa-download"></i>
             Export
           </button>
 
-          <h1 class="title">Flush Data</h1>
+          <h2>Flush Data</h2>
+          <p class="is-size-6">
+            Flush all the data and reload the app.
+          </p>
           <button class="button is-danger" @click="flushData">
-            <i class="fa fa-fw fa-plus"></i>
+            <i class="fa fa-fw fa-trash-alt"></i>
             Flush Data
           </button>
 
@@ -67,6 +76,9 @@
 
 <script>
 const DownloadFile = require('downloadjs')
+const storeModules = [
+  <%- helpers.indent(blueprint.schemas.map(s => `'${s.identifier}'`).join(',\n'), 2) %>
+]
 
 export default {
   name: 'Settings',
@@ -75,30 +87,63 @@ export default {
   },
   data () {
     return {
-      importJson: {},
-      imported: false,
-      importValid: false
+      importSuccess: false,
+      importError: false
     }
   },
   methods: {
     processFile (event) {
+      // Resets the component's state
+      this.importSuccess = false
+      this.importError = false
+
+      // Attempts to load the file from the <input type='file' />
       const file = event.target.files[0]
       const reader = new FileReader()
-      // TODO - add a try/catch statement here
-      reader.onload = (e) => {
-        this.imported = true
-        this.importJson = JSON.parse(e.target.result)
-        if (Object.keys(this.importJson)[0]) {
-          this.importValid = true
-          window.localStorage.vuex = JSON.stringify(this.importJson)
-          window.location = '/' // Sends back to home page
+
+      // Wraps to catch any errors
+      try {
+
+        // Defines an onload callback for the file reader
+        reader.onload = (e) => {
+
+          // Attempt to parse file's contents as JSON
+          // NOTE - this will throw an error if it's invalid
+          const importedJson = JSON.parse(e.target.result)
+
+          // Iterates over each storeModule, commits the changes to the store if data is defined
+          storeModules.forEach(each => {
+            if (importedJson[each] && Array.isArray(importedJson[each])) {
+              this.$store.commit(`${each}/collection/items`, importedJson[each])
+            }
+          })
+
+          // Assuming there's no error, indicate success
+          this.importSuccess = true
+
+          // Sends back to home page to refresh
+          window.location = '/'
         }
+
+        // Attempts to read the file as text
+        reader.readAsText(file)
+
+      // Handle import error
+      } catch (e) {
+        this.importSuccess = false
+        this.importError = true
       }
-      reader.readAsText(file)
     },
     exportData() {
-      const state = this.$store.state
-      DownloadFile(JSON.stringify(state, null, 2), "<%= blueprint.identifier %>_export.json", 'application/json')
+      // Assmbles the data being exported
+      const state = {}
+      storeModules.forEach(each => state[each] = this.$store.state[each].collection.items)
+
+      // Assembles the filename
+      const filename = `<%= blueprint.identifier %>_export_${new Date().getTime()}.json`
+
+      // Downloads the file
+      DownloadFile(JSON.stringify(state, null, 2), filename, 'application/json')
     },
     flushData() {
       delete window.localStorage.vuex // Flushes vuex state in localStorage
@@ -107,3 +152,4 @@ export default {
   }
 }
 </script>
+
