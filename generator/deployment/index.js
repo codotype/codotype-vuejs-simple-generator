3372a8e6c704d32b3640bfe7ff14d2ed71623e44
Alexander Schwartzberg <aeksco@gmail.com>
2019-07-07 16:45:32 -0400

    Bug fix in deployment generator, netlify.toml working

--- generator/deployment/index.js
+++ generator/deployment/index.js
@@ -2,7 +2,12 @@
 module.exports = {
   name: 'Deployment',
   async write({ configuration }) {
-    if (!configuration.deployment.netlify) {
+    console.log('configuration.deployment.netlify')
+    console.log('configuration.deployment.netlify')
+    console.log(configuration.deployment.netlify)
+    console.log(configuration.deployment.netlify)
+    console.log(configuration.deployment.netlify)
+    if (configuration.deployment.netlify) {
       await this.renderComponent({
         src: 'netlify.toml',
         dest: 'netlify.toml'
